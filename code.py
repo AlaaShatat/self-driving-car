0236d92e3293bd840319c7c17f6dc5a7f802ed6d
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2022-04-20 14:05:43 +0200

    Update code.py

--- code.py
+++ code.py
@@ -203,3 +203,125 @@ class LaneLines:
         lR, rR, pos = self.measure_curvature()
 
         return out_img
+
+
+    def plot(self, out_img):
+        np.set_printoptions(precision=6, suppress=True)
+        lR, rR, pos = self.measure_curvature()
+
+        value = None
+        if abs(self.left_fit[0]) > abs(self.right_fit[0]):
+            value = self.left_fit[0]
+        else:
+            value = self.right_fit[0]
+
+        if abs(value) <= 0.00015:
+            self.dir.append('F')
+        elif value < 0:
+            self.dir.append('L')
+        else:
+            self.dir.append('R')
+        
+        if len(self.dir) > 10:
+            self.dir.pop(0)
+
+        W = 400
+        H = 500
+        widget = np.copy(out_img[:H, :W])
+        widget //= 2
+        widget[0,:] = [0, 0, 255]
+        widget[-1,:] = [0, 0, 255]
+        widget[:,0] = [0, 0, 255]
+        widget[:,-1] = [0, 0, 255]
+        out_img[:H, :W] = widget
+
+        direction = max(set(self.dir), key = self.dir.count)
+        msg = "Keep Straight Ahead"
+        curvature_msg = "Curvature = {:.0f} m".format(min(lR, rR))
+        
+        if direction == 'L':
+        
+            y, x = self.left_curve_img[:,:,3].nonzero()
+            out_img[y, x-100+W//2] = self.left_curve_img[y, x, :3]
+            msg = "Left Curve Ahead"
+            
+        if direction == 'R':
+            y, x = self.right_curve_img[:,:,3].nonzero()
+            out_img[y, x-100+W//2] = self.right_curve_img[y, x, :3]
+            msg = "Right Curve Ahead"
+        if direction == 'F':
+            y, x = self.keep_straight_img[:,:,3].nonzero()
+            out_img[y, x-100+W//2] = self.keep_straight_img[y, x, :3]
+
+        cv2.putText(out_img, msg, org=(10, 240), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+        
+        if direction in 'LR':
+            cv2.putText(out_img, curvature_msg, org=(10, 280), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+
+        cv2.putText(
+            out_img,
+            "Good Lane Keeping",
+            org=(10, 400),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=1.2,
+            color=(0, 255, 0),
+            thickness=2)
+
+        cv2.putText(
+            out_img,
+            "Vehicle is {:.2f} m away from center".format(pos),
+            org=(10, 450),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.66,
+            color=(255, 255, 255),
+            thickness=2)
+        
+        return out_img
+
+    def measure_curvature(self):
+        ym = 30/720
+        xm = 3.7/700
+
+        left_fit = self.left_fit.copy()
+        right_fit = self.right_fit.copy()
+        y_eval = 700 * ym
+
+        # Compute R_curve (radius of curvature)
+        left_curveR =  ((1 + (2*left_fit[0] *y_eval + left_fit[1])**2)**1.5)  / np.absolute(2*left_fit[0])
+        right_curveR = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+
+        xl = np.dot(self.left_fit, [700**2, 700, 1])
+        xr = np.dot(self.right_fit, [700**2, 700, 1])
+        pos = (1280//2 - (xl+xr)//2)*xm
+        return left_curveR, right_curveR, pos 
+
+#threshold
+def threshold_rel(img, lo, hi):
+    vmin = np.min(img)
+    vmax = np.max(img)
+    vlo = vmin + (vmax - vmin) * lo
+    vhi = vmin + (vmax - vmin) * hi
+    return np.uint8((img >= vlo) & (img <= vhi)) * 255
+
+def threshold_abs(img, lo, hi):
+    return np.uint8((img >= lo) & (img <= hi)) * 255
+#main thresholding  
+def thresholding (img1):
+    hls = cv2.cvtColor(img1, cv2.COLOR_RGB2HLS)
+    hsv = cv2.cvtColor(img1, cv2.COLOR_RGB2HSV)
+    h_channel = hls[:,:,0]
+    l_channel = hls[:,:,1]
+    s_channel = hls[:,:,2]
+    v_channel = hsv[:,:,2]
+  
+
+    right_lane = threshold_rel(l_channel, 0.8, 1.0)
+    right_lane[:,:750] = 0
+
+    left_lane = threshold_abs(h_channel, 20, 30)
+    left_lane &= threshold_rel(v_channel, 0.7, 1.0)
+    left_lane[:,550:] = 0
+    img2 = left_lane | right_lane
+    x=  threshold_rel(l_channel, 0.8, 1.0)
+    x = np.dstack((x,x,x))
+    return img2, x
