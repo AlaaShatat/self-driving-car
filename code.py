e1e3ca9c3312e88ba463bdf2eae6b9794fef4c42
Alaa Shatat <alaashabaan102210@gnail.com>
2022-04-20 07:48:27 +0200

    lane-line, test images folder

---
+++ code.py
@@ -0,0 +1,106 @@
+# import part
+import sys
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from moviepy.editor import VideoFileClip
+
+#  arguments 
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+mode = sys.argv[3]
+
+
+
+
+# lane line detection 
+def hist(img):
+    bottom_half = img[img.shape[0]//2:,:]
+    return np.sum(bottom_half, axis=0)
+
+class LaneLines:
+    """ Class containing information about detected lane lines.
+
+    Attributes used:
+        left_fit (np.array): Coefficients of a polynomial that fit left lane line
+        right_fit (np.array): Coefficients of a polynomial that fit right lane line
+        parameters (dict): Dictionary containing all parameters needed for the pipeline
+        debug (boolean): Flag for debug/normal mode
+    """
+    def __init__(self):
+        """Init Lanelines.
+
+        Parameters:
+            left_fit (np.array): Coefficients of polynomial that fit left lane
+            right_fit (np.array): Coefficients of polynomial that fit right lane
+            binary (np.array): binary image
+        """
+        self.left_fit = None
+        self.right_fit = None
+        self.binary = None
+        self.nonzero = None
+        self.nonzerox = None
+        self.nonzeroy = None
+        self.clear_visibility = True
+        self.dir = []
+        self.left_curve_img = mpimg.imread('left_turn.png')
+        self.right_curve_img = mpimg.imread('right_turn.png')
+        self.keep_straight_img = mpimg.imread('straight.png')
+        self.left_curve_img = cv2.normalize(src=self.left_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.right_curve_img = cv2.normalize(src=self.right_curve_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        self.keep_straight_img = cv2.normalize(src=self.keep_straight_img, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+        # HYPERPARAMETERS
+        # Number of sliding windows
+        self.nwindows = 9
+        # Width of the the windows +/- margin
+        self.margin = 100
+        # Mininum number of pixels found to recenter window
+        self.minpix = 50
+
+    def forward(self, img):
+        """Take a image and detect lane lines.
+
+        Parameters:
+            img (np.array): An binary image containing relevant pixels
+
+        Returns:
+            Image (np.array): An RGB image containing lane lines pixels and other details
+        """
+        self.extract_features(img)
+        return self.fit_poly(img)
+
+    def pixels_in_window(self, center, margin, height):
+        """ Return all pixel that in a specific window
+
+        Parameters:
+            center (tuple): coordinate of the center of the window
+            margin (int): half width of the window
+            height (int): height of the window
+
+        Returns:
+            pixelx (np.array): x coordinates of pixels that lie inside the window
+            pixely (np.array): y coordinates of pixels that lie inside the window
+        """
+        topleft = (center[0]-margin, center[1]-height//2)
+        bottomright = (center[0]+margin, center[1]+height//2)
+
+        condx = (topleft[0] <= self.nonzerox) & (self.nonzerox <= bottomright[0])
+        condy = (topleft[1] <= self.nonzeroy) & (self.nonzeroy <= bottomright[1])
+        return self.nonzerox[condx&condy], self.nonzeroy[condx&condy]
+
+    def extract_features(self, img):
+        """ Extract features from a binary image
+
+        Parameters:
+            img (np.array): A binary image
+        """
+        self.img = img
+        # Height of of windows - based on nwindows and image shape
+        self.window_height = np.int(img.shape[0]//self.nwindows)
+
+        # Identify the x and y positions of all nonzero pixel in the image
+        self.nonzero = img.nonzero()
+        self.nonzerox = np.array(self.nonzero[1])
+        self.nonzeroy = np.array(self.nonzero[0])
